0dfa3f63d5095ff6ffedc68582d9deaea850ea98
Marco Nilsson <marco@zyax.se>
2025-03-11 10:40:22 +0100

    Fix typo in URL filter.

--- src/flashback.js
+++ src/flashback.js
@@ -8,7 +8,7 @@ chrome.storage.local.get([MUTED_USERS_KEY], (data) => {
     // Ignore everything for now if we're not reading a thread. This fixes the
     // issue where search results are collapsable. This will be removed when
     // the extension coverage is extended.
-    if (!location.pathname.startsWith("/p"))
+    if (!location.pathname.startsWith("/t"))
         return;
 
     initStorage(data);
